db6481678061722654a9384e983afcc04e737a84
Deja <hooddeja43@gmail.com>
2025-10-07 18:13:39 -0400

    Update murder-mystery-game.sql

--- murder-mystery-game.sql
+++ murder-mystery-game.sql
@@ -43,7 +43,30 @@ SELECT * FROM get_fit_now_member
 WHERE id = '48Z7A' OR id = '48Z55';
 
 --From this there were only 2 suspects (Jeremy Bowers and Joe Germuska) and so I plugged in both answers/names into the solution and Jeremy Bowers turned out to be the killer!
+--In interview he said the following: -I was hired by a woman with a lot of money. 
+--  I don't know her name but I know she's around 5'5" (65") or 5'7" (67"). 
+---  She has red hair and she drives a Tesla Model S. 
+ -- I know that she attended the SQL Symphony Concert 3 times in December 2017.
+--So I looked up that info in driving records:
+SELECT *
+FROM drivers_license
+WHERE hair_color = 'red'
+  AND height BETWEEN 65 AND 67
+  AND car_make = 'Tesla'
+  AND car_model = 'Model S';
 
+
+--And then from there I looked up the license_id's from there and person_id numbers:
+SELECT *
+FROM person
+WHERE license_id IN (423327, 423384);
+
+--The following people came up (Red Korb, Regina George, and Miranda Priestly ) and then I crossed referenced them with the people that attended the Symphony 3 times:
+SELECT * 
+FROM facebook_event_checkin
+WHERE event_name = 'SQL Symphony Concert'
+
+--This showed Regina George so shes the hitman/ person who hired Jeremy!!
 
 
 
